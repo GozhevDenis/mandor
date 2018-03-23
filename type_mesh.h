a27c564dee662e5aa32a6e8f9095a98dc17a1779
DNKOsiris <dnkosiris@gmail.com>
2018-03-23 13:11:33 +0300

    Доработки

--- type_mesh.h
+++ type_mesh.h
@@ -1,7 +1,7 @@
   //file type_mesh.h
 #pragma once
 #include <iostream>
-#include <vector>
+//#include <vector>
 
 ///#include <stdio.h>					//доделать
 ///#include <string.h>
@@ -12,24 +12,33 @@ template <typename T>
 class mesh_t
 {
 private:
-	int imin, imax;
-	int jmin, jmax;
-	int kmin, kmax;
 	int width_z;
 	int width_yz;
-	std::vector<T> storage;
+	T *storage;
 	int origin;
 	std::string name;
 
-	void init();										//width initialization and and memory allocation
+	void init();										//width initialization and memory allocation
 public:
+	int imin, imax;
+	int jmin, jmax;
+	int kmin, kmax;
+
 	mesh_t();
 	mesh_t(int, int, int, int, int, int, const char*);
+	~mesh_t();
+
+	//mesh_t(const mesh_t&)							//доделать
+	//mesh_& operator=(const mesh_t&)				//или запретить
+
 	explicit mesh_t(int [3], int[3], const char*);
 	void free();										////нужен ли этот метод?
 	void resize(int, int, int, int, int, int);
 	///void save();
 	///void load();
+
+	T& operator()(int i, int j, int k);
+	const T& operator()(int i, int j, int k) const;
 
 	template <typename U> friend std::ostream& operator << (std::ostream&, const mesh_t<U>&);
 
