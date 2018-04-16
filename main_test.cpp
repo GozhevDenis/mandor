2fc85529815f152e91209410c15140b9775a12bc
DNKOsiris <dnkosiris@gmail.com>
2018-04-16 19:00:31 +0300

    ошибка линковшика в mesh

--- main_test.cpp
+++ main_test.cpp
@@ -1,8 +1,16 @@
 #include "easylogging\easylogging++.h"
+#include "type_vector.h"
+#include "type_mesh.h"
+
+#include <iostream>
 
 INITIALIZE_EASYLOGGINGPP
 
 int main()
 {
+	//mesh_t<vec3d_t> vec(0, 0, 0, 10, 10, 10, "name");
+	mesh_t<double> vec(0, 0, 0, 10, 10, 10, "name");
+	//std::cout << vec(5, 4, 3).v.x << std::endl;
+	system("pause");
 	return 0;
 }
